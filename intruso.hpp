0b11d86fad13f212a02de109c581dea0de5d7519
drinhope <pedroalonso2507@gmail.com>
2022-10-25 14:47:34 -0300

    correção hpp/cpp

---
+++ intruso.hpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+
+class Intruso{
+    /*Continue a implementação da classe Intruso*/
+
+    public:
+        void set_senha_vazada(std::string vazou);
+        std::string crack_senha();
+};
