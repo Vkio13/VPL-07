7205aa5aa49e83516bfaae508301b8aa2e9eac1e
Vkio13 <111925541+Vkio13@users.noreply.github.com>
2022-11-01 20:03:08 -0300

    Update intruso.hpp

--- intruso.hpp
+++ intruso.hpp
@@ -1,17 +1,12 @@
-#ifndef INTRUSO
-#define INTRUSO
-
 #include <iostream>
 #include <string>
 #include <map>
 #include <vector>
 
 class Intruso{
-    std::vector<int> numeros;
-    std::map<char,numeros> digitos;
+    private:
+        std::vector<std::vector<char>>digitadas;
     public:
         void set_senha_vazada(std::string vazou);
         std::string crack_senha();
 };
-
-#endif
